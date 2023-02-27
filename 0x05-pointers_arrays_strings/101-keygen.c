2762b1ab169c9a7287db4f5fdef34dfde827ad0c
israeldeho <mrkwamedzifa@gmail.com>
2023-02-27 00:34:08 -0800

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,26 +10,34 @@
  */
 int main(void)
 {
-	int pass[100];
-	int i, sum, n;
+    char pass[100];
+    int i, sum, n;
 
- sum i = 0;	
+    sum = 0;
 
-	srand(time(NULL));
+    srand(time(NULL));
 
-	for (i = 0; i < 100; i++)
-	{
-		pass[i] = rand() % 78;
-		sum += (pass[i] + '0');
-		putchar(pass[i] + '0');
-		if ((2772 - sum) - '0' < 78)
-		{
-			n = 2772 - sum - '0';
-			sum += n;
-			putchar(n + '0');
-			break;
-		}
-	}
+    for (i = 0; i < 100; i++)
+    {
+        pass[i] = rand() % 78 + '0';
+        sum += pass[i];
+        putchar(pass[i]);
+    }
 
-	return (0);
+    n = (2772 - sum) % 78;
+
+    if (n < 10)
+    {
+        pass[0] += n;
+        putchar(pass[0]);
+    }
+    else
+    {
+        pass[0] += n / 10;
+        pass[1] += n % 10;
+        putchar(pass[0]);
+        putchar(pass[1]);
+    }
+
+    return (0);
 }
