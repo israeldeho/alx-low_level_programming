507bd243d8487cb06e07c370f404c05fb9734c18
israeldeho <mrkwamedzifa@gmail.com>
2023-03-06 07:42:43 -0800

    main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -11,7 +11,7 @@
  * enclosing the files  contentinbetween ifndef,define and endif,
  * stops the compiler from processing the file twice if its included twice,
  * if not so, this will cause an error
- * */
+ */
 
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
